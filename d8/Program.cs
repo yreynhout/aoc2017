720a6857611966cb978a1afe0ad02c618608e092
Yves Reynhout <yves.reynhout@gmail.com>
2017-12-15 16:52:11 +0100

    Completed day 8

--- d8/Program.cs
+++ d8/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Sprache;
 
 namespace d8
@@ -7,36 +10,86 @@ namespace d8
     {
         static void Main(string[] args)
         {
-            try
-            {
-                var result = RegisterInstructionParser.Instruction.Parse("b inc -5");
-                Console.WriteLine("{0} {1} {2}", result.Register, result.Operation, result.Amount);
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e);
-            }
-        }
-    }
-
-    public class AllRegisters
-    {
-
-    }
-
-    public class RegisterInstruction
-    {
-        public RegisterInstruction(string register, string operation, int amount, Predicate<AllRegisters> condition)
-        {
-            
+            var parser = RegisterInstructionParser.Instance;
+            var input = File.ReadAllLines("input.txt");
+            var instructions = Array.ConvertAll(
+                input,
+                line => {
+                    Console.WriteLine(line);
+                    return parser.Parse(line);
+                }
+            );
+            var registers = new AllRegisters();
+            foreach(var instruction in instructions)
+                instruction.Execute(registers);
+            Console.WriteLine(registers.MaximumValue);
         }
     }
 
     public enum Operation { Increment, Decrement }
-    public enum Comparison { GreaterThan, GreaterThanOrEqual, LessThan, LessThanOrEqual }
+    public enum Comparison { GreaterThan, GreaterThanOrEqual, LessThan, LessThanOrEqual, Equal, NotEqual }
 
     // b inc 5 if a > 1
     // action(register operation number) "if" condition(register comparison-operator number)
+
+    public class AllRegisters
+    {
+        private Dictionary<string, int> _registers = new Dictionary<string, int>();
+
+        public int MaximumValue 
+        {
+            get 
+            {
+                return _registers.Values.Max();
+            }
+        }
+
+        public int GetRegister(string register)
+        {
+            if(_registers.TryGetValue(register, out int value))
+            {
+                return value;
+            }
+            return 0;
+        }
+
+        public void SetRegister(string register, int value)
+        {
+            if(_registers.ContainsKey(register))
+            {
+                _registers[register] = value;
+            }
+            else
+            {
+                _registers[register] = value;
+            }
+        }
+    }
+
+    public class RegisterInstruction
+    {
+        public RegisterInstruction(Instruction instruction, Condition condition)
+        {
+            Instruction = instruction;
+            Condition = condition;
+        }
+
+        public Instruction Instruction { get; }
+        public Condition Condition { get; }
+
+        public void Execute(AllRegisters registers)
+        {
+            if(Condition.IsSatisfiedBy(registers))
+            {
+                Instruction.Execute(registers);
+            }
+        }
+
+        public override string ToString()
+        {
+            return Instruction.Register + " " + Instruction.Operation + " " + Instruction.Amount + " if " + Condition.Register + " " + Condition.Comparison + " " + Condition.Amount;
+        }
+    }
 
     public class Instruction
     {
@@ -50,6 +103,20 @@ namespace d8
         public string Register { get; }
         public Operation Operation { get; }
         public int Amount { get; }
+
+        public void Execute(AllRegisters registers)
+        {
+            var value = registers.GetRegister(Register);
+            switch(Operation)
+            {
+                case Operation.Increment:
+                    registers.SetRegister(Register, value + Amount);
+                    break;
+                case Operation.Decrement:
+                    registers.SetRegister(Register, value - Amount);
+                    break;
+            }
+        }
     }
 
     public class Condition
@@ -64,69 +131,81 @@ namespace d8
         public string Register { get; }
         public Comparison Comparison { get; }
         public int Amount { get; }
+
+        public bool IsSatisfiedBy(AllRegisters registers)
+        {
+            var value = registers.GetRegister(Register);
+            var result = false;
+            switch(Comparison)
+            {
+                case Comparison.Equal:
+                    result = value == Amount;
+                    break;
+                case Comparison.NotEqual:
+                    result = value != Amount;
+                    break;
+                case Comparison.GreaterThan:
+                    result = value > Amount;
+                    break;
+                case Comparison.GreaterThanOrEqual:
+                    result = value >= Amount;
+                    break;
+                case Comparison.LessThan:
+                    result = value < Amount;
+                    break;
+                case Comparison.LessThanOrEqual:
+                    result = value <= Amount;
+                    break;
+            }
+            return result;
+        }
     }
 
     public static class RegisterInstructionParser
     {
-        static readonly Parser<string> Register = Parse.Identifier(Parse.Letter, Parse.Letter);
+        public static readonly Parser<string> Register = Parse.Letter.AtLeastOnce().Token().Text();
 
-        static Parser<Operation> Operator(string input, Operation output)
+        public static Parser<Operation> Operator(string input, Operation output)
         {
             return Parse.String(input).Token().Return(output);
         }
 
-        static readonly Parser<Operation> Inc = Operator("inc", Operation.Increment);
-        static readonly Parser<Operation> Dec = Operator("dec", Operation.Decrement);
+        public static readonly Parser<Operation> Inc = Operator("inc", Operation.Increment);
+        public static readonly Parser<Operation> Dec = Operator("dec", Operation.Decrement);
 
-        static readonly Parser<int> Amount =
+        public static readonly Parser<int> Amount =
             from sign in Parse.Char('-').Optional()
             from number in Parse.Number
             select sign.IsDefined ? -Convert.ToInt32(number) : Convert.ToInt32(number);
 
         public static readonly Parser<Instruction> Instruction =
             from register in Register
-            from operation in Inc.XOr(Dec)
+            from operation in Inc.Or(Dec).Token()
             from amount in Amount
             select new Instruction(register, operation, amount);
 
-        static Parser<Comparison> Comparator(string input, Comparison output)
+        public static Parser<Comparison> Comparator(string input, Comparison output)
         {
             return Parse.String(input).Token().Return(output);
         }
 
-        static readonly Parser<Comparison> GreaterThan = Comparator(">", Comparison.GreaterThan);
-        static readonly Parser<Comparison> GreaterThanOrEqual = Comparator(">=", Comparison.GreaterThanOrEqual);
-        static readonly Parser<Comparison> LessThan = Comparator("<", Comparison.LessThan);
-        static readonly Parser<Comparison> LessThanOrEqual = Comparator("<=", Comparison.LessThanOrEqual);
+        public static readonly Parser<Comparison> Equal = Comparator("==", Comparison.Equal);
+        public static readonly Parser<Comparison> NotEqual = Comparator("!=", Comparison.NotEqual);
+        public static readonly Parser<Comparison> GreaterThan = Comparator(">", Comparison.GreaterThan);
+        public static readonly Parser<Comparison> GreaterThanOrEqual = Comparator(">=", Comparison.GreaterThanOrEqual);
+        public static readonly Parser<Comparison> LessThan = Comparator("<", Comparison.LessThan);
+        public static readonly Parser<Comparison> LessThanOrEqual = Comparator("<=", Comparison.LessThanOrEqual);
 
         public static readonly Parser<Condition> Condition =
             from register in Register
-            from comparison in GreaterThan.XOr(LessThan).XOr(GreaterThanOrEqual).XOr(LessThanOrEqual)
+            from comparison in Equal.Or(NotEqual).Or(GreaterThanOrEqual).Or(GreaterThan).Or(LessThanOrEqual).Or(LessThan).Token()
             from amount in Amount
             select new Condition(register, comparison, amount);
 
-        
-        // public static readonly Parser<Operation> Operation()
-        // {
-        //     Parse.Number
-        //     var whitespace = Parse.WhiteSpace.Except(Parse.LineEnd);
-        //     return from leading in whitespace.Many()
-        //            from item in OperationIdentifier
-        //            from trailing in whitespace.Many()
-        //         select item == 'inc;
-        // }
-//         public static readonly Parser<IEnumerable<RegisterInstruction>> Assembler = (
-//             from register in Register
-//             from operation in Operation.Optional()
-//             from amount in AsmToken(Comment.SingleLineComment).Optional()
-//             from lineTerminator in Parse.LineTerminator
-//             select new RegisterInstruction(
-//                 label.GetOrDefault(),
-//                 instruction.IsEmpty ? null : instruction.Get().Item1,
-//                 instruction.IsEmpty ? null : instruction.Get().Item2,
-//                 comment.GetOrDefault()
-//                 )
-// ).XMany().End();
-
+        public static readonly Parser<RegisterInstruction> Instance =
+            from instruction in Instruction
+            from literal in Parse.String("if").Token()
+            from condition in Condition
+            select new RegisterInstruction(instruction, condition);
     }
 }
